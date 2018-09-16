d7df85b1cf1eae0f7d4930945b65115e3c7a355a
davidflores10 <davidflores10.df@gmail.com>
2018-09-16 18:16:48 +0200

    First neural network example

--- System/views.py
+++ System/views.py
@@ -1,7 +1,12 @@
 # Create your views here.
+
+
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
 from django.template import Context, loader
+from numpy.ma import array
+
+from System.neural_network import NeuralNetwork
 
 
 def index(request):
@@ -9,4 +14,12 @@ def index(request):
 
 
 def neural(request):
-    return HttpResponse('hello world')
+    neuralNetwork = NeuralNetwork()
+
+    # variables
+    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+    training_set_outputs = array([[0, 1, 1, 0]]).T
+
+    neuralNetwork.train(training_set_inputs, training_set_outputs, 1000)
+
+    return HttpResponse(neuralNetwork.think(array([1, 0, 0])))
